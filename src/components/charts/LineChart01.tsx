72d031ea9de78ac50a3294c7b6410b7982cf19d4
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-01 20:00:54 -0300

    feat(chart): create LineChart01

---
+++ src/components/charts/LineChart01.tsx
@@ -0,0 +1,90 @@
+import React, { useRef, useEffect } from 'react'
+
+import {
+  Chart,
+  LineController,
+  LineElement,
+  Filler,
+  PointElement,
+  LinearScale,
+  TimeScale,
+  Tooltip,
+} from 'chart.js'
+import 'chartjs-adapter-moment'
+import { formatValue } from '@/utils/Utils'
+
+Chart.register(
+  LineController,
+  LineElement,
+  Filler,
+  PointElement,
+  LinearScale,
+  TimeScale,
+  Tooltip
+)
+
+interface LineChartProps {
+  data: any
+  width: number
+  height: number
+}
+
+function LineChart01({ data, width, height }: LineChartProps) {
+  const canvas = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    const ctx = canvas?.current
+    if (!ctx) return
+
+    // eslint-disable-next-line no-unused-vars
+    const chart = new Chart(ctx, {
+      type: 'line',
+      data: data,
+      options: {
+        // chartArea: {
+        //   backgroundColor: '#f8fafc',
+        // },
+        layout: {
+          padding: 20,
+        },
+        scales: {
+          y: {
+            display: false,
+            beginAtZero: true,
+          },
+          x: {
+            type: 'time',
+            time: {
+              parser: 'MM-DD-YYYY',
+              unit: 'month',
+            },
+            display: false,
+          },
+        },
+        plugins: {
+          tooltip: {
+            callbacks: {
+              title: () => '', // Disable tooltip title
+              label: (context) => formatValue(context.parsed.y),
+            },
+          },
+          legend: {
+            display: false,
+          },
+        },
+        interaction: {
+          intersect: false,
+          mode: 'nearest',
+        },
+        maintainAspectRatio: false,
+        resizeDelay: 200,
+      },
+    })
+    return () => chart.destroy()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return <canvas ref={canvas} width={width} height={height}></canvas>
+}
+
+export default LineChart01
