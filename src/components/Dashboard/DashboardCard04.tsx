4220454b42bf664dcfd2d0b1aaae395ff52070b4
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-02 20:04:26 -0300

    feat(chart): test `DashboardCard04`

---
+++ src/components/Dashboard/DashboardCard04.tsx
@@ -0,0 +1,56 @@
+import BarChart from '../charts/BarChart01'
+import {
+  ChartContainer,
+  DashBoardCard,
+  DashBoardCardHeader,
+  DashBoardCardTitle,
+} from './styles'
+
+// Import utilities
+
+function DashboardCard04() {
+  const chartData = {
+    labels: [
+      '12-01-2022',
+      '01-01-2023',
+      '02-01-2023',
+      '03-01-2023',
+      '04-01-2023',
+      '05-01-2023',
+    ],
+    datasets: [
+      // Light blue bars
+      {
+        label: 'Direto',
+        data: [800, 1600, 900, 1300, 1950, 1700],
+        backgroundColor: '#60a5fa',
+        hoverBackgroundColor: '#3b82f6',
+        barPercentage: 0.66,
+        categoryPercentage: 0.66,
+      },
+      // Blue bars
+      {
+        label: 'Indireto',
+        data: [4900, 2600, 5350, 4800, 5200, 4800],
+        backgroundColor: '#6366f1',
+        hoverBackgroundColor: '#2563eb',
+        barPercentage: 0.66,
+        categoryPercentage: 0.66,
+      },
+    ],
+  }
+
+  return (
+    <DashBoardCard>
+      <DashBoardCardHeader>
+        <DashBoardCardTitle>Direto vs Indireto</DashBoardCardTitle>
+      </DashBoardCardHeader>
+
+      <ChartContainer>
+        <BarChart data={chartData} width={595} height={248} />
+      </ChartContainer>
+    </DashBoardCard>
+  )
+}
+
+export default DashboardCard04
