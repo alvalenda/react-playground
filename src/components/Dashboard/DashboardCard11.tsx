467d486c13dcdf855739ef6fe2f75a7bccf7c787
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-03 18:39:30 -0300

    feat(dash): create `DashboardCard11`

--- src/components/Dashboard/DashboardCard11.tsx
+++ src/components/Dashboard/DashboardCard11.tsx
@@ -1,6 +1,63 @@
 import styled from 'styled-components'
 import BarChart from '../charts/BarChart03'
 
+const DashBoardCard = styled.div`
+  background-color: #ffffff;
+  border: 1px solid #e5e7eb;
+  border-radius: 0.125rem;
+  grid-column: 1 / -1;
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+    0 4px 6px -2px rgba(0, 0, 0, 0.05);
+
+  z-index: 2;
+
+  @media (min-width: 1280px) {
+    grid-column: span 6 / span 6;
+  }
+`
+
+const ChartHeader = styled.header`
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  padding-left: 1.25rem;
+  padding-right: 1.25rem;
+  border-bottom: 1px solid #f1f5f9;
+`
+const ChartH2 = styled.h2`
+  font-weight: 600;
+  color: #1e293b;
+`
+const ChartPadding = styled.div`
+  padding-top: 0.75rem;
+  padding-bottom: 0.75rem;
+  padding-left: 1.25rem;
+  padding-right: 1.25rem;
+`
+const ChartFlexItemContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+`
+const ChartCounter = styled.div`
+  margin-right: 0.5rem;
+  font-size: 1.875rem;
+  line-height: 2.25rem;
+  font-weight: 700;
+  color: #1e293b;
+`
+const ChartTag = styled.div`
+  padding-left: 0.375rem;
+  padding-right: 0.375rem;
+  background-color: #f59e0b;
+  color: #ffffff;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  font-weight: 600;
+  border-radius: 9999px;
+`
+export const ChartContainer = styled.div`
+  flex-grow: 1;
+`
+
 function DashboardCard11() {
   const chartData = {
     labels: ['Produto'],
@@ -68,60 +125,3 @@ function DashboardCard11() {
 }
 
 export default DashboardCard11
-
-const DashBoardCard = styled.div`
-  background-color: #ffffff;
-  border: 1px solid #e5e7eb;
-  border-radius: 0.125rem;
-  grid-column: 1 / -1;
-  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
-    0 4px 6px -2px rgba(0, 0, 0, 0.05);
-
-  z-index: 2;
-
-  @media (min-width: 1280px) {
-    grid-column: span 6 / span 6;
-  }
-`
-
-const ChartHeader = styled.header`
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-  padding-left: 1.25rem;
-  padding-right: 1.25rem;
-  border-bottom: 1px solid #f1f5f9;
-`
-const ChartH2 = styled.h2`
-  font-weight: 600;
-  color: #1e293b;
-`
-const ChartPadding = styled.div`
-  padding-top: 0.75rem;
-  padding-bottom: 0.75rem;
-  padding-left: 1.25rem;
-  padding-right: 1.25rem;
-`
-const ChartFlexItemContainer = styled.div`
-  display: flex;
-  align-items: flex-start;
-`
-const ChartCounter = styled.div`
-  margin-right: 0.5rem;
-  font-size: 1.875rem;
-  line-height: 2.25rem;
-  font-weight: 700;
-  color: #1e293b;
-`
-const ChartTag = styled.div`
-  padding-left: 0.375rem;
-  padding-right: 0.375rem;
-  background-color: #f59e0b;
-  color: #ffffff;
-  font-size: 0.875rem;
-  line-height: 1.25rem;
-  font-weight: 600;
-  border-radius: 9999px;
-`
-export const ChartContainer = styled.div`
-  flex-grow: 1;
-`
